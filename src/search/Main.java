bbba98e8e832f87123c8121db765f75d6bb4dc6a
eunhee <dmsgml9903@gmail.com>
2023-10-04 16:22:29 +0900

    06 중복 문자열 제거 - 해답

--- src/search/Main.java
+++ src/search/Main.java
@@ -1,51 +1,22 @@
 package search;
-
 import java.util.Scanner;
 
 public class Main {
 
   public static String solution(String str) {
-
-    int lt = 0;
-    int rt = str.length() - 1;
-
-    char[] cArray = str.toCharArray();
-    while (lt < rt) {
-
-      boolean ltFlag = true;
-      boolean rtFlag = true;
-      while (ltFlag || rtFlag) {
-        if (!((cArray[lt] > 64 && cArray[lt] < 91) || (cArray[lt] > 96 && cArray[lt] < 123))) {
-          lt++;
-        }else {
-          ltFlag = false;
-        }
-        if (!((cArray[rt] > 64 && cArray[rt] < 91) || (cArray[rt] > 96 && cArray[rt] < 123))) {
-          rt--;
-        }else{
-          rtFlag = false;
-        }
-      }
-
-      char tmp = cArray[lt];
-      cArray[lt] = cArray[rt];
-      cArray[rt] = tmp;
-
-      lt++;
-      rt--;
+    StringBuilder sb = new StringBuilder();
+    for(int i=0; i<str.length(); i++) {
+      if(str.indexOf(str.charAt(i))==i)
+        sb.append(str.charAt(i));
     }
-
-    return String.valueOf(cArray);
-
+    return sb.toString();
   }
 
-  public static void main(String[] args) {
+  public static void main(String[] args){
+    Scanner in=new Scanner(System.in);
+    String str = in.next();
 
-    Scanner in = new Scanner(System.in);
-    String input = in.next();
-    System.out.println(solution(input));
-
+    System.out.println(solution(str));
+    return ;
   }
-
 }
-
--- src/search/string/RemoveDuplicated.java
+++ src/search/string/RemoveDuplicated.java
@@ -5,24 +5,18 @@ import java.util.Scanner;
 public class RemoveDuplicated {
 
   public static String solution(String str) {
-
     StringBuilder sb = new StringBuilder();
-    char[] cArray = str.toCharArray();
-    for (int i = 0; i < str.length(); i++) {
-      if (str.indexOf(cArray[i]) == i) {
-        sb.append(cArray[i]);
-      }
+    for(int i=0; i<str.length(); i++) {
+      if(str.indexOf(str.charAt(i))==i)
+        sb.append(str.charAt(i));
     }
-
     return sb.toString();
-
   }
 
-
-  public static void main(String[] args) {
-    Scanner in = new Scanner(System.in);
+  public static void main(String[] args){
+    Scanner in=new Scanner(System.in);
     String str = in.next();
     System.out.println(solution(str));
+    return ;
   }
-
 }
