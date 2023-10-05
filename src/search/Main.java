b1490fd387b0f9f08976a5eb119ec94a1d61afd1
eunhee <dmsgml9903@gmail.com>
2023-10-05 12:36:25 +0900

    09 숫자만 추출 - 해답2

--- src/search/Main.java
+++ src/search/Main.java
@@ -1,22 +1,21 @@
 package search;
 
 import java.util.Scanner;
+
 public class Main {
 
-  public static int solution(String str) {
-
-    int ans = 0;
-    for (char c : str.toCharArray()) {
-      if (c >= 48 && c <= 57) {
-        ans = ans * 10 + (c - 48);
-      }
+  public static String solution(String str) {
+    StringBuilder sb = new StringBuilder();
+    for(int i=0; i<str.length(); i++) {
+      if(str.indexOf(str.charAt(i)) == i)
+        sb.append(str.charAt(i));
     }
-    return ans;
+    return sb.toString();
   }
-  public static void main(String[] args) {
-    Scanner in = new Scanner(System.in);
+  public static void main(String[] args){
+    Scanner in=new Scanner(System.in);
     String input = in.next();
     System.out.println(solution(input));
+    return ;
   }
-
 }
