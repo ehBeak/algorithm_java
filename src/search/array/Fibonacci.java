4b7865937ed7c120f8f87c903921654bf31f6bad
eunhee <dmsgml9903@gmail.com>
2023-10-10 17:46:30 +0900

    2.4 피보나치 수열 - 배열 없이

--- src/search/array/Fibonacci.java
+++ src/search/array/Fibonacci.java
@@ -4,23 +4,25 @@ import java.util.*;
 
 public class Fibonacci {
 
-  public static int[] solution(int n) {
-    int[] ans = new int[n];
-    for (int i = 0; i < n; i++) {
-      if (i == 0 || i == 1) {
-        ans[i] = 1;
-      } else {
-        ans[i] = ans[i - 1] + ans[i - 2];
-      }
+  public static void solution(int n) {
+    int first_prev = 1;
+    int second_prev = 1;
+    int tmp;
+    System.out.print("1 1 ");
+    for (int i = 2; i < n; i++) {
+      tmp = first_prev + second_prev;
+      System.out.print(tmp + " ");
+      second_prev = first_prev;
+      first_prev = tmp;
     }
-    return ans;
   }
 
   public static void main(String[] args) {
     Scanner in = new Scanner(System.in);
     int n = in.nextInt();
-    for (int i : solution(n)) {
-      System.out.print(i + " ");
-    }
+    solution(n);
+//    for (int i : solution(n)) {
+//      System.out.print(i + " ");
+//    }
   }
 }
