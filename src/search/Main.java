6ee5a92259fb8cc833ca51254c3724f4995c4a2e
eunhee <dmsgml9903@gmail.com>
2023-10-10 20:15:30 +0900

    2.6 소수 뒤집 - 해답

--- src/search/Main.java
+++ src/search/Main.java
@@ -4,36 +4,38 @@ import java.util.*;
 
 public class Main {
 
-  public static List<Integer> solution(String[] strArray, int n) {
-
-    int[] reverseNumArray = new int[n];
-    List<Integer> ans = new ArrayList<>();
-    boolean flag;
-
-    for (int i = 0; i < n; i++)
-      reverseNumArray[i] = Integer.parseInt(new StringBuilder(strArray[i]).reverse().toString());
-
-
-    for (int i = 0; i < n; i++) {
-      int tmp = reverseNumArray[i];
-      flag = true;
-      if (tmp == 1) flag = false;
-
-      for (int j = 2; j*j <= tmp; j++)
-        if (tmp % j == 0) flag = false;
-
-      if (flag) ans.add(tmp);
+  public static boolean isPrime(int num) {
+    if (num == 1) {
+      return false;
     }
+    for (int i = 2; i*i <= num; i++) {
+      if (num % i == 0) {
+        return false;
+      }
+    }
+    return true;
+  }
 
+  public static List<Integer> solution(int[] iArray, int n) {
+    List<Integer> ans = new ArrayList<>();
+    for (int i = 0; i < n; i++) {
+      int tmp = iArray[i];
+      int res = 0;
+      while (tmp > 0) {
+        res = res * 10 + (tmp % 10);
+        tmp = tmp / 10;
+      }
+      if (isPrime(res)) ans.add(res);
+    }
     return ans;
   }
 
   public static void main(String[] args) {
     Scanner in = new Scanner(System.in);
     int n = in.nextInt();
-    String[] input = new String[n];
+    int[] input = new int[n];
     for(int i=0; i<n; i++) {
-      input[i] = in.next();
+      input[i] = in.nextInt();
     }
 
     for (int i : solution(input, n)) {
