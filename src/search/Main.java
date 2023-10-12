814b841a761fc13b382112d3643ab7b8288e743a
eunhee <dmsgml9903@gmail.com>
2023-10-12 12:15:22 +0900

    2.10 봉우 - 해답

--- src/search/Main.java
+++ src/search/Main.java
@@ -5,13 +5,21 @@ import java.util.*;
 public class Main {
 
   public static int solution(int[][] arr, int n) {
+    int[] dx = {-1, 0, 1, 0};
+    int[] dy = {0, 1, 0, -1};
     int cnt = 0;
-    for (int i = 1; i < n + 1; i++) {
-      for (int j = 1; j < n + 1; j++) {
-        int tmp = arr[i][j];
-        if ((arr[i - 1][j] < tmp) && (arr[i][j - 1] < tmp) && (arr[i + 1][j] < tmp) && (arr[i][j + 1] < tmp)) {
-          cnt++;
+    for (int i = 0; i < n; i++) {
+      for (int j = 0; j < n; j++) {
+        boolean flag = true;
+        for (int k = 0; k < 4; k++) {
+          int nx = dx[k];
+          int ny = dy[k];
+          if (nx < n && nx >=0 && ny < n && ny >= 0 && arr[i + nx][j + ny] >= arr[i][j]) {
+            flag = true;
+            break;
+          }
         }
+        if(flag) cnt++;
       }
     }
     return cnt;
@@ -20,10 +28,10 @@ public class Main {
   public static void main(String[] args) {
     Scanner in = new Scanner(System.in);
     int n = in.nextInt();
-    int[][] arr = new int[n + 2][n + 2];
+    int[][] arr = new int[n][n];
 
-    for (int i = 1; i < n + 1; i++) {
-      for (int j = 1; j < n + 1; j++) {
+    for (int i = 0; i < n; i++) {
+      for (int j = 0; j < n; j++) {
         arr[i][j] = in.nextInt();
       }
     }
