14411a77c2211bee2347693bc00b01959ecae26e
eunhee <dmsgml9903@gmail.com>
2023-10-13 14:36:41 +0900

    2.11 임시 반장 정하기 - 해답

--- src/search/Main.java
+++ src/search/Main.java
@@ -5,37 +5,43 @@ import java.util.*;
 public class Main {
 
   public static int solution(int[][] arr, int n) {
-    int[] dx = {-1, 0, 1, 0};
-    int[] dy = {0, 1, 0, -1};
-    int cnt = 0;
-    for (int i = 0; i < n; i++) {
-      for (int j = 0; j < n; j++) {
-        boolean flag = true;
-        for (int k = 0; k < 4; k++) {
-          int nx = dx[k];
-          int ny = dy[k];
-          if (nx < n && nx >=0 && ny < n && ny >= 0 && arr[i + nx][j + ny] >= arr[i][j]) {
-            flag = true;
+
+    int max = Integer.MIN_VALUE;
+    int ans = 0;
+
+    for (int i = 1; i <= n; i++) {
+      int cnt = 0;
+      for (int j = 1; j <= n; j++) {
+        for (int k = 1; k <= 5; k++) {
+          if (arr[i][k] == arr[j][k]) {
+            cnt++;
             break;
           }
         }
-        if(flag) cnt++;
+      }
+      if (max < cnt) {
+        max = cnt;
+        ans = i;
       }
     }
-    return cnt;
+
+    return ans;
   }
 
   public static void main(String[] args) {
     Scanner in = new Scanner(System.in);
     int n = in.nextInt();
-    int[][] arr = new int[n][n];
 
-    for (int i = 0; i < n; i++) {
-      for (int j = 0; j < n; j++) {
+    int[][] arr = new int[n + 1][6];
+
+    for (int i = 1; i <= n; i++) {
+      for (int j = 1; j <= 5; j++) {
         arr[i][j] = in.nextInt();
       }
     }
+
     System.out.println(solution(arr, n));
   }
+
 }
 
