16e23a907bfae1365cd4b8b6e9a5f39e71b4a252
eunhee <dmsgml9903@gmail.com>
2023-10-17 03:32:43 +0900

    2.10 봉우리 - 2번째

--- src/search/Main.java
+++ src/search/Main.java
@@ -6,34 +6,36 @@ public class Main {
 
   public static int solution(int[][] arr, int n) {
 
-    int[] idx = {-1, 0, 1, 0};
-    int[] jdx = {0, -1, 0, 1};
+    int[] dx = {1, 0, -1, 0};
+    int[] dy = {0, -1, 0, 1};
 
-    int res = 0;
+    int cnt = 0;
 
     for (int i = 0; i < n; i++) {
       for (int j = 0; j < n; j++) {
         boolean flag = true;
         for (int k = 0; k < 4; k++) {
-          int x = i + idx[k];
-          int y = j + jdx[k];
-          if (x < n && x >= 0 && y < n && y >= 0 && arr[i][j] <= arr[x][y]) {
+          int x = i + dx[k];
+          int y = j + dy[k];
+          if (x >= 0 && x < n && y >= 0 && y < n && arr[i][j] <= arr[x][y]) {
             flag = false;
             break;
           }
         }
-        if (flag) res++;
-
+        if (flag) cnt++;
       }
     }
-    return res;
+
+    return cnt;
 
   }
+
   public static void main(String[] args){
+
     Scanner in = new Scanner(System.in);
     int n = in.nextInt();
-    int[][] arr = new int[n][n];
 
+    int[][] arr = new int[n][n];
     for (int i = 0; i < n; i++) {
       for (int j = 0; j < n; j++) {
         arr[i][j] = in.nextInt();
