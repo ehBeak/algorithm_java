e389f45abcbc0251d947599bfeefa14afd0e4381
eunhee <dmsgml9903@gmail.com>
2023-10-17 04:19:13 +0900

    2.06 중복 문자열 - 6번째

--- src/search/Main.java
+++ src/search/Main.java
@@ -4,42 +4,20 @@ import java.util.*;
 
 public class Main {
 
-  public static int solution(int arr[][], int n, int m) {
-    int res = 0;
-    int pi = 0;
-    int pj = 0;
-    for (int i = 1; i <= n; i++) {
-      for (int j = 1; j <= n; j++) {
-        int cnt = 0;
-        for (int k = 0; k < m; k++) {
-          for (int s = 0; s < n; s++) {
-            if (arr[k][s] == i) pi = s;
-            if (arr[k][s] == j) pj = s;
-          }
-          if (pi < pj) cnt++;
-        }
-        if (cnt == m) res++;
-
-      }
+  public static String solution(String str) {
+    StringBuilder sb = new StringBuilder();
+    for(int i=0; i<str.length(); i++) {
+      if(str.indexOf(str.charAt(i)) == i)
+        sb.append(str.charAt(i));
     }
-    return res;
-
+    return sb.toString();
   }
 
-
-  public static void main(String[] args) {
-
-    Scanner in = new Scanner(System.in);
-    int n = in.nextInt();
-    int m = in.nextInt();
-
-    int[][] arr = new int[m][n];
-    for (int i = 0; i < m; i++) {
-      for (int j = 0; j < n; j++) {
-        arr[i][j] = in.nextInt();
-      }
-    }
-    System.out.println(solution(arr, n, m));
+  public static void main(String[] args){
+    Scanner in=new Scanner(System.in);
+    String input = in.next();
+    System.out.println(solution(input));
+    return ;
   }
 }
 
