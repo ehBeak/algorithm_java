0b2f203ece8d576609a6dcf81e5344b8a5a5ce42
eunhee <dmsgml9903@gmail.com>
2023-10-17 04:56:00 +0900

    3.01 두 배열 합치 - 해답

--- src/search/Main.java
+++ src/search/Main.java
@@ -4,35 +4,17 @@ import java.util.*;
 
 public class Main {
 
-  public static int[] solution(int n, int[] arr1, int m, int[] arr2) {
-    int[] res = new int[n + m];
-    int idx1 = 0;
-    int idx2 = 0;
-
-    for (int i = 0; i < n + m; i++) {
-      if (idx1 >= n) {
-        while (idx2 < m) {
-          res[i++] = arr2[idx2];
-          idx2++;
-        }
-        break;
-      }
-      if (idx2 >= m) {
-        while (idx1 < n) {
-          res[i++] = arr2[idx1];
-          idx1++;
-        }
-        break;
-      }
-
-      if (arr1[idx1] > arr2[idx2]) {
-        res[i] = arr2[idx2];
-        idx2++;
-      } else {
-        res[i] = arr1[idx1];
-        idx1++;
-      }
+  public static ArrayList<Integer> solution(int n, int[] arr1, int m, int[] arr2) {
+    ArrayList<Integer> res = new ArrayList<>();
+    int p1 = 0;
+    int p2 = 0;
+    while (p1 < n && p2 < m) {
+      if (arr1[p1] > arr2[p2]) res.add(arr2[p2++]);
+      else res.add(arr1[p1++]);
     }
+
+    while (p1 < n) res.add(arr1[p1++]);
+    while (p2 < m) res.add(arr2[p2++]);
 
     return res;
   }
