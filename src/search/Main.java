c27b218f851364cff9d15a581bed26d723c3aaf7
eunhee <dmsgml9903@gmail.com>
2023-10-17 04:04:33 +0900

    2.12 멘토링 - 1번째

--- src/search/Main.java
+++ src/search/Main.java
@@ -4,43 +4,42 @@ import java.util.*;
 
 public class Main {
 
-  public static int solution(int[][] arr, int n) {
+  public static int solution(int arr[][], int n, int m) {
     int res = 0;
-    int max = Integer.MIN_VALUE;
-
-
+    int pi = 0;
+    int pj = 0;
     for (int i = 1; i <= n; i++) {
-      int cnt = 0;
       for (int j = 1; j <= n; j++) {
-        for (int k = 1; k <= 5; k++) {
-          if(arr[i][k]==arr[j][k]) {
-            cnt++;
-            break;
+        int cnt = 0;
+        for (int k = 0; k < m; k++) {
+          for (int s = 0; s < n; s++) {
+            if (arr[k][s] == i) pi = s;
+            if (arr[k][s] == j) pj = s;
           }
+          if (pi < pj) cnt++;
         }
-      }
-      if(cnt > max) {
-        res = i;
-        max = cnt;
+        if (cnt == m) res++;
+
       }
     }
     return res;
-  }
 
+  }
 
 
   public static void main(String[] args) {
 
     Scanner in = new Scanner(System.in);
     int n = in.nextInt();
-    int[][] arr = new int[n+1][6];
-    for (int i = 1; i <= n; i++) {
-      for (int j = 1; j < 6; j++) {
+    int m = in.nextInt();
+
+    int[][] arr = new int[m][n];
+    for (int i = 0; i < m; i++) {
+      for (int j = 0; j < n; j++) {
         arr[i][j] = in.nextInt();
       }
     }
-
-    System.out.println(solution(arr, n));
+    System.out.println(solution(arr, n, m));
   }
 }
 
