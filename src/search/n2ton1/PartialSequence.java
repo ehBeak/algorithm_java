037f44446e8c7ba687d4f004a96b8a9f933b92bd
eunhee <dmsgml9903@gmail.com>
2023-10-18 14:46:14 +0900

    3.4 연속 부분수열 해답

--- src/search/n2ton1/PartialSequence.java
+++ src/search/n2ton1/PartialSequence.java
@@ -1,37 +1,40 @@
-package search;
+package search.n2ton1;
 
-import java.util.Scanner;
+import java.util.*;
 
-public class Main {
+public class PartialSequence {
 
   public static int solution(int[] arr, int n, int m) {
-    int lt = 0, sum = 0, cnt = 0;
+
+    int ans = 0, sum = 0, lt = 0;
     for (int rt = 0; rt < n; rt++) {
       sum += arr[rt];
       if (sum == m) {
-        cnt++;
+        ans++;
       }
       while (sum >= m) {
         sum -= arr[lt++];
         if (sum == m) {
-          cnt++;
+          ans++;
         }
       }
-    }
 
-    return cnt;
+    }
+    return ans;
   }
-  public static void main(String[] args){
+
+  public static void main(String[] args) {
+
     Scanner in = new Scanner(System.in);
     int n = in.nextInt();
     int m = in.nextInt();
+
     int[] arr = new int[n];
     for (int i = 0; i < n; i++) {
       arr[i] = in.nextInt();
     }
 
     System.out.println(solution(arr, n, m));
-
-    return ;
   }
+
 }
