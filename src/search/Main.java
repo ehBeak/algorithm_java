6b27fc91d1aefdc2e20fc163d0a9983e06e32970
eunhee <dmsgml9903@gmail.com>
2023-10-19 23:16:03 +0900

    2.10 봉우리

--- src/search/Main.java
+++ src/search/Main.java
@@ -3,18 +3,42 @@ package search;
 import java.util.Scanner;
 
 public class Main {
-  public static String solution(String str) {
-    StringBuilder sb = new StringBuilder();
-    for(int i=0; i<str.length(); i++) {
-      if(str.indexOf(str.charAt(i))==i)
-        sb.append(str.charAt(i));
+
+  public static int solution(int[][] arr, int n) {
+
+    int[] dx = {0, 1, 0, -1};
+    int[] dy = {1, 0, -1, 0};
+
+    int cnt = 0;
+
+    for (int i = 0; i < n; i++) {
+      for (int j = 0; j < n; j++) {
+        boolean flag = true;
+        for (int k = 0; k < 4; k++) {
+          int x = i + dx[k];
+          int y = j + dy[k];
+          if (x >= 0 && x < n && y >= 0 && y < n && arr[i][j] <= arr[x][y]) {
+            flag = false;
+            break;
+          }
+        }
+        if (flag) cnt++;
+      }
     }
-    return sb.toString();
+    return cnt;
   }
   public static void main(String[] args){
-    Scanner in=new Scanner(System.in);
-    String input = in.next();
-    System.out.println(solution(input));
+    Scanner in = new Scanner(System.in);
+    int n = in.nextInt();
+    int[][] arr = new int[n][n];
+
+    for (int i = 0; i < n; i++) {
+      for (int j = 0; j < n; j++) {
+        arr[i][j] = in.nextInt();
+      }
+    }
+
+    System.out.println(solution(arr, n));
     return ;
   }
 }
