6d74062aeb284e533705db662f39ebb836a1a2e6
eunhee <dmsgml9903@gmail.com>
2023-10-20 00:41:35 +0900

    3.05 연속된 자연수의 합 - 해답

--- src/search/Main.java
+++ src/search/Main.java
@@ -1,40 +1,31 @@
 package search;
 
 import java.util.*;
-
 public class Main {
 
-  public static int solution(int[] arr, int n, int m) {
+  public static int solution(int n) {
+    int res = 0;
+    int lt = 1, sum = 0;
+    for (int rt = 1; rt <= (n / 2 + 1); rt++) {
+      sum += rt;
+      if (sum == n) res++;
 
-    int ans = 0, sum = 0, lt = 0;
-    for (int rt = 0; rt < n; rt++) {
-      sum += arr[rt];
-      if (sum == m) {
-        ans++;
-      }
-      while (sum >= m) {
-        sum -= arr[lt++];
-        if (sum == m) {
-          ans++;
+      while (sum >= n) {
+        sum -= lt++;
+        if (sum == n) {
+          res++;
         }
       }
-
     }
-    return ans;
+    return res;
   }
+
 
   public static void main(String[] args) {
 
     Scanner in = new Scanner(System.in);
     int n = in.nextInt();
-    int m = in.nextInt();
-
-    int[] arr = new int[n];
-    for (int i = 0; i < n; i++) {
-      arr[i] = in.nextInt();
-    }
-
-    System.out.println(solution(arr, n, m));
+    System.out.println(solution(n));
   }
 
 }
