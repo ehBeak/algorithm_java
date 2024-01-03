f2385b9a457adf7e55534eecc67fc6a843e54691
eunhee <dmsgml9903@gmail.com>
2024-01-03 20:34:26 +0900

    1-6 중복 문자 제거

--- src/search/Main.java
+++ src/search/Main.java
@@ -4,27 +4,14 @@ import java.util.Scanner;
 
 public class Main {
 
-  public static char[] solution(String input) {
-    int lt = 0;
-    int rt = input.length() - 1;
-    char[] cArray = input.toCharArray();
-
-    while (lt < rt) {
-      if (Character.isAlphabetic(cArray[lt]) && Character.isAlphabetic(cArray[rt])) {
-        char tmp = cArray[lt];
-        cArray[lt] = cArray[rt];
-        cArray[rt] = tmp;
-        lt++;
-        rt--;
-      }
-      if (!Character.isAlphabetic(cArray[lt])) {
-        lt++;
-      }
-      if (!Character.isAlphabetic(cArray[rt])) {
-        rt--;
+  public static String solution(String input) {
+    StringBuilder sb = new StringBuilder();
+    for (int i = 0; i < input.length(); i++) {
+      if (input.indexOf(input.charAt(i)) == i) {
+        sb.append(input.charAt(i));
       }
     }
-    return cArray;
+    return sb.toString();
   }
 
   public static void main(String[] args){
