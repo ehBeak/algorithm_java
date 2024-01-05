87adee3f6dc50a9de8e4aa147c835a92a15b722f
eunhee <dmsgml9903@gmail.com>
2024-01-05 23:24:30 +0900

    4.01 학급회장 정답(해쉬)

--- src/search/map/ClassPresident.java
+++ src/search/map/ClassPresident.java
@@ -6,18 +6,16 @@ import java.util.Scanner;
 
 public class ClassPresident {
 
-    public static char solution(int num, String str) {
+    public static char solution(String str) {
         Map<Character, Integer> map = new HashMap<>();
-        for (int i = 0; i < num; i++) {
-            Character key = str.charAt(i);
-            Integer integer = map.getOrDefault(key, 0);
-            map.put(str.charAt(i), ++integer);
+        for (char key : str.toCharArray()) {
+            map.put(key, map.getOrDefault(key, 0) + 1);
         }
 
-        Integer max = 0;
+        int max = Integer.MIN_VALUE;
         Character maxKey = null;
         for (Character key : map.keySet()) {
-            Integer tmp = map.get(key);
+            int tmp = map.get(key);
             if (max < tmp) {
                 max = tmp;
                 maxKey = key;
@@ -30,6 +28,6 @@ public class ClassPresident {
         Scanner in = new Scanner(System.in);
         int num = in.nextInt();
         String input = in.next();
-        System.out.println(solution(num, input));
+        System.out.println(solution(input));
     }
 }
