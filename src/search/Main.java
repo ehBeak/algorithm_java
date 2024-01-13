f76394a6f4109d14512ab526bb3205b11472aaee
eunhee <dmsgml9903@gmail.com>
2024-01-13 17:10:56 +0900

    4.02 아나그램 복습1

--- src/search/Main.java
+++ src/search/Main.java
@@ -1,39 +1,33 @@
 package search;
 
+import java.util.HashMap;
+import java.util.Map;
 import java.util.Scanner;
 
 public class Main {
 
-  public static int solution(int[][] arr, int n) {
-    int[] idx = {0, 1, 0, -1};
-    int[] jdx = {1, 0, -1, 0};
-    boolean flag = true;
-    int count = 0;
-    for (int i = 1; i <= n; i++) {
-      for (int j = 1; j <= n; j++) {
-        flag = true;
-        for (int k = 0; k < 4; k++) {
-          if (arr[i][j] <= arr[i + idx[k]][j + jdx[k]]) {
-            flag = false;
-          }
-        }
-        if (flag) {
-          count++;
-        }
-      }
+  private static final String YES = "YES";
+  private static final String NO = "NO";
+
+  public static String solution(String str1, String str2) {
+    Map<Character, Integer> map = new HashMap<>();
+    for (Character key : str1.toCharArray()) {
+      map.put(key, map.getOrDefault(key, 0) + 1);
     }
-    return count;
+
+    for (Character key : str2.toCharArray()) {
+      if (!map.containsKey(key) || map.get(key) == 0) {
+        return NO;
+      }
+      map.put(key, map.get(key) - 1);
+    }
+    return YES;
   }
 
   public static void main(String[] args){
     Scanner in = new Scanner(System.in);
-    int n = in.nextInt();
-    int[][] arr = new int[n + 2][n + 2];
-    for (int i = 1; i <= n; i++) {
-      for (int j = 1; j <= n; j++) {
-        arr[i][j] = in.nextInt();
-      }
-    }
-    System.out.println(solution(arr, n));
+    String str1 = in.next();
+    String str2 = in.next();
+    System.out.println(solution(str1, str2));
   }
 }
