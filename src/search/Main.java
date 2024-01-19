b701c951c24312bcd7f2726e4f859456af873a6a
eunhee <dmsgml9903@gmail.com>
2024-01-19 19:09:59 +0900

    4.3 매출액의 종류 - 복습 2회

--- src/search/Main.java
+++ src/search/Main.java
@@ -1,41 +1,42 @@
 package search;
 
 import java.util.Scanner;
-import java.util.Map;
+import java.util.List;
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.Map;
 
 public class Main {
 
-  public static int solution(String s, String t) {
-    Map<Character, Integer> sMap = new HashMap<>();
-    Map<Character, Integer> tMap = new HashMap<>();
-    int answer = 0;
-    for (Character c : t.toCharArray()) {
-      tMap.put(c, tMap.getOrDefault(c, 0) + 1);
+  public static List<Integer> solution(int[] arr, int n, int k) {
+    List<Integer> list = new ArrayList<>();
+    Map<Integer, Integer> map = new HashMap<>();
+    for (int i = 0; i < k - 1; i++) {
+      map.put(arr[i], map.getOrDefault(arr[i], 0) + 1);
     }
 
-    for (int i = 0; i < t.length() - 1; i++) {
-      sMap.put(s.charAt(i), sMap.getOrDefault(s.charAt(i), 0) + 1);
-    }
-
-    int k = t.length() - 1;
-    for (int i = t.length() - 1; i < s.length(); i++) {
-      sMap.put(s.charAt(i), sMap.getOrDefault(s.charAt(i), 0) + 1);
-      if (sMap.equals(tMap)) {
-        answer++;
-      }
-      sMap.put(s.charAt(i - k), sMap.get(s.charAt(i - k)) - 1);
-      if (sMap.get(s.charAt(i - k)) == 0) {
-        sMap.remove(s.charAt(i - k));
+    for (int i = k - 1; i < n; i++) {
+      map.put(arr[i], map.getOrDefault(arr[i], 0) + 1);
+      list.add(map.size());
+      map.put(arr[i - k + 1], map.get(arr[i - k + 1]) - 1);
+      if (map.get(arr[i - k + 1]) == 0) {
+        map.remove(arr[i - k + 1]);
       }
     }
-    return answer;
+    return list;
   }
 
   public static void main(String[] args){
     Scanner in = new Scanner(System.in);
-    String s = in.next();
-    String t = in.next();
-    System.out.println(solution(s, t));
+    int n = in.nextInt();
+    int[] arr = new int[n];
+    int k = in.nextInt();
+    for (int i = 0; i < n; i++) {
+      arr[i] = in.nextInt();
+    }
+
+    for (Integer i : solution(arr, n, k)) {
+      System.out.print(i + " ");
+    }
   }
 }
