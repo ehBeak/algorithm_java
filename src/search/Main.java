02f932a694d2ad5124b0c0993beab38b7931e3e0
eunhee <dmsgml9903@gmail.com>
2024-01-20 16:59:11 +0900

    5.6 공주구하기 - 복습1

--- src/search/Main.java
+++ src/search/Main.java
@@ -1,42 +1,34 @@
 package search;
 
 import java.util.Scanner;
-import java.util.List;
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.Map;
+import java.util.Queue;
+import java.util.LinkedList;
 
 public class Main {
 
-  public static List<Integer> solution(int[] arr, int n, int k) {
-    List<Integer> list = new ArrayList<>();
-    Map<Integer, Integer> map = new HashMap<>();
-    for (int i = 0; i < k - 1; i++) {
-      map.put(arr[i], map.getOrDefault(arr[i], 0) + 1);
+  public static int solution(int n, int k) {
+    Queue<Integer> queue = new LinkedList<>();
+    for (int i = 1; i <= n; i++) {
+      queue.offer(i);
     }
 
-    for (int i = k - 1; i < n; i++) {
-      map.put(arr[i], map.getOrDefault(arr[i], 0) + 1);
-      list.add(map.size());
-      map.put(arr[i - k + 1], map.get(arr[i - k + 1]) - 1);
-      if (map.get(arr[i - k + 1]) == 0) {
-        map.remove(arr[i - k + 1]);
+    int number = 1;
+    while (queue.size() != 1) {
+      if (number == k) {
+        queue.poll();
+        number = 1;
+      } else {
+        queue.offer(queue.poll());
+        number++;
       }
     }
-    return list;
+    return queue.poll();
   }
 
   public static void main(String[] args){
     Scanner in = new Scanner(System.in);
     int n = in.nextInt();
-    int[] arr = new int[n];
     int k = in.nextInt();
-    for (int i = 0; i < n; i++) {
-      arr[i] = in.nextInt();
-    }
-
-    for (Integer i : solution(arr, n, k)) {
-      System.out.print(i + " ");
-    }
+    System.out.println(solution(n, k));
   }
 }
