3fda87a5d81c8c7b1f9ab335e7985fdc7aabf881
eunhee <dmsgml9903@gmail.com>
2024-01-22 23:13:40 +0900

    5.6 공주 구하기 - 복습2

--- src/search/Main.java
+++ src/search/Main.java
@@ -5,50 +5,26 @@ import java.util.Queue;
 import java.util.LinkedList;
 
 public class Main {
-  class Person {
-    public Integer id;
-    public Integer priority;
 
-    public Person(Integer id, Integer priority) {
-      this.id = id;
-      this.priority = priority;
-    }
-  }
-
-  public int solution(int[] arr, int n, int m) {
-    int number = 0;
-    Queue<Person> queue = new LinkedList<>();
-    for (int i = 0; i < n; i++) {
-      queue.offer(new Person(i, arr[i]));
+  public static int solution(int n, int k) {
+    Queue<Integer> queue = new LinkedList<>();
+    for (int i = 1; i <= n; i++) {
+      queue.offer(i);
     }
 
-    while (true) {
-      Person person = queue.poll();
-      boolean now = true;
-      for (Person p : queue) {
-        if (person.priority < p.priority) {
-          now = false;
-        }
+    while (queue.size() != 1) {
+      for (int i = 0; i < k-1; i++) {
+        queue.offer(queue.poll());
       }
-      if (now) {
-        number++;
-        if (person.id == m) {
-          return number;
-        }
-      } else {
-        queue.offer(person);
-      }
+      queue.poll();
     }
+    return queue.poll();
   }
 
   public static void main(String[] args){
     Scanner in = new Scanner(System.in);
     int n = in.nextInt();
-    int m = in.nextInt();
-    int[] arr = new int[n];
-    for (int i = 0; i < n; i++) {
-      arr[i] = in.nextInt();
-    }
-    System.out.println(new Main().solution(arr, n, m));
+    int k = in.nextInt();
+    System.out.println(solution(n, k));
   }
 }
