48b6a7954b253a8366602b9733ea0dff6e2d31ca
eunhee <dmsgml9903@gmail.com>
2024-01-22 23:24:54 +0900

    3.4 연속 부분 수열 - 복습1 (해답)

--- src/search/Main.java
+++ src/search/Main.java
@@ -1,30 +1,36 @@
 package search;
 
 import java.util.Scanner;
-import java.util.Queue;
-import java.util.LinkedList;
 
 public class Main {
 
-  public static int solution(int n, int k) {
-    Queue<Integer> queue = new LinkedList<>();
-    for (int i = 1; i <= n; i++) {
-      queue.offer(i);
-    }
-
-    while (queue.size() != 1) {
-      for (int i = 0; i < k-1; i++) {
-        queue.offer(queue.poll());
+  public static int solution(int[] arr, int n, int m) {
+    int answer = 0;
+    int sum = 0;
+    int idx = 0;
+    for (int i = 0; i < n; i++) {
+      sum += arr[i];
+      if (sum == m) {
+        answer++;
       }
-      queue.poll();
+      while (sum >= m) {
+        sum -= arr[idx++];
+        if (sum == m) {
+          answer++;
+        }
+      }
     }
-    return queue.poll();
+    return answer;
   }
 
   public static void main(String[] args){
     Scanner in = new Scanner(System.in);
     int n = in.nextInt();
-    int k = in.nextInt();
-    System.out.println(solution(n, k));
+    int m = in.nextInt();
+    int[] arr = new int[n];
+    for (int i = 0; i < n; i++) {
+      arr[i] = in.nextInt();
+    }
+    System.out.println(solution(arr, n, m));
   }
 }
