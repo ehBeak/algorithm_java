14fe297a3ab41a7c71f1baff913695a1f9b89bcc
eunhee <dmsgml9903@gmail.com>
2024-01-23 23:14:22 +0900

    3.4 연속 부분수열 - 복습2

--- src/algorithm/Main.java
+++ src/algorithm/Main.java
@@ -1,46 +1,27 @@
 package algorithm;
 
 import java.util.Scanner;
-import java.util.Queue;
-import java.util.LinkedList;
 
-class Person {
-  public int id;
-  public int priority;
-
-  public Person(int id, int priority) {
-    this.id = id;
-    this.priority = priority;
-  }
-}
 public class Main {
 
   public static int solution(int[] arr, int n, int m) {
-    // m번째 환자 저장
-    Queue<Person> queue = new LinkedList<>();
-    for (int i = 0; i < n; i++) {
-      queue.offer(new Person(i, arr[i]));
+    int sum = 0;
+    int number = 0;
+    int lt = 0;
+    for (int rt = 0; rt < n; rt++) {
+      sum += arr[rt];
+      if (sum == m) {
+        number++;
+      }
+      while (sum >= m) {
+        sum -= arr[lt++];
+        if (sum == m) {
+          number++;
+        }
+      }
     }
 
-    int number = 0;
-    while (true) {
-      Person person = queue.poll();
-      boolean now = true;
-      for (Person p : queue) {
-        if (person.priority < p.priority) {
-          now = false;
-          break;
-        }
-      }
-      if (now) {
-        number++;
-        if (person.id == m) {
-          return number;
-        }
-      }else{
-        queue.offer(person);
-      }
-    }
+    return number;
   }
 
   public static void main(String[] args){
