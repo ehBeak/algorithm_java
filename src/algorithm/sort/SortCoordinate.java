543147a9c675dadac9bbb824f10307ece579073b
eunhee <dmsgml9903@gmail.com>
2024-01-23 17:31:27 +0900

    6.7 좌표 정렬하기

---
+++ src/algorithm/sort/SortCoordinate.java
@@ -0,0 +1,47 @@
+package algorithm.sort;
+
+import java.util.Scanner;
+import java.util.List;
+import java.util.ArrayList;
+import java.util.Collections;
+
+public class SortCoordinate {
+
+    public static class Pair implements Comparable<Pair> {
+        public int x;
+        public int y;
+
+        public Pair(int x, int y) {
+            this.x = x;
+            this.y = y;
+        }
+
+        @Override
+        public int compareTo(Pair o) {
+            if (this.x < o.x) {
+                return -1;
+            }
+            if (this.x == o.x && this.y < o.y) {
+                return -1;
+            }
+            return 1;
+        }
+    }
+
+    public static List<Pair> solution(List<Pair> list, int n) {
+        Collections.sort(list);
+        return list;
+    }
+
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        int n = in.nextInt();
+        List<Pair> list = new ArrayList<>();
+        for (int i = 0; i < n; i++) {
+            list.add(new Pair(in.nextInt(), in.nextInt()));
+        }
+        for (Pair p : solution(list, n)) {
+            System.out.println(p.x + " " + p.y);
+        }
+    }
+}
