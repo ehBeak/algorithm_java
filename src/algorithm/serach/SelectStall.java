08bfa987763720450e49e0dea3aec05065fc05e0
eunhee <dmsgml9903@gmail.com>
2024-01-25 23:32:52 +0900

    6.10 마구간 정하기 - 오답

---
+++ src/algorithm/serach/SelectStall.java
@@ -0,0 +1,47 @@
+package algorithm.serach;
+
+import java.util.Scanner;
+import java.util.Arrays;
+
+public class SelectStall {
+
+    public static int countHorse(int[] arr, int n, int maxDistance) {
+        int stall = arr[0];
+        int number = 1;
+        for (int i = 0; i < n; i++) {
+            if (arr[i] - stall > maxDistance) {
+                number++;
+                stall = arr[i];
+            }
+        }
+        return number;
+    }
+
+    public static int solution(int[] arr, int n, int horseCnt) {
+        int maxDistance = 0;
+        Arrays.sort(arr);
+        int lt = arr[0];
+        int rt = arr[n - 1] - arr[0];
+        while (lt <= rt) {
+            int mid = (lt + rt) / 2;
+            if (countHorse(arr, n, mid) >= horseCnt) {
+                lt = mid + 1;
+                maxDistance = mid;
+            } else {
+                rt = mid - 1;
+            }
+        }
+        return maxDistance;
+    }
+
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        int n = in.nextInt();
+        int horseCnt = in.nextInt();
+        int[] arr = new int[n];
+        for (int i = 0; i < n; i++) {
+            arr[i] = in.nextInt();
+        }
+        System.out.println(solution(arr, n, horseCnt));
+    }
+}
