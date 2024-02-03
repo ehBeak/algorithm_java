98f2bda276406def607da310f518d607966a3a71
eunhee <dmsgml9903@gmail.com>
2024-02-03 22:03:59 +0900

    7.7 bfs

--- src/algorithm/bfs/BFS.java
+++ src/algorithm/bfs/BFS.java
@@ -18,24 +18,24 @@ public class BFS {
 
     public void bfs(Node root) {
         Queue<Node> queue = new LinkedList<>();
-        queue.offer(root); // root값 넣기
+        queue.offer(root);
         int level = 0;
         while (!queue.isEmpty()) {
+            System.out.print(level++ + " : ");
             int size = queue.size();
-            System.out.print(level + " : ");
-            for (int i = 0; i < size; i++) { // 현재 가지고 있는 노드의 data 빼가면서 출력하기
+            for (int i = 0; i < size; i++) {
                 Node curNode = queue.poll();
                 System.out.print(curNode.data + " ");
-                if (curNode.lt != null) { // curNode의 왼쪽에 값이 존재한다면(말단 노드가 아니라면)
+                if (curNode.lt != null) {
                     queue.offer(curNode.lt);
                 }
-                if (curNode.rt != null) { // curNode의 오른쪽에 값이 존재한다면(말단 노드가 아니라면)
+                if (curNode.rt != null) {
                     queue.offer(curNode.rt);
                 }
             }
-            level++;
             System.out.println();
         }
+
     }
 
     public static void main(String[] args) {
