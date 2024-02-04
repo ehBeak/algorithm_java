33027040e85add0d18b1dc9fd751f5d97acb7fa7
eunhee <dmsgml9903@gmail.com>
2024-02-04 19:47:08 +0900

    7.1 재귀함수 - 복습

--- src/algorithm/recursive/Recursive.java
+++ src/algorithm/recursive/Recursive.java
@@ -4,19 +4,29 @@ import java.util.Scanner;
 
 public class Recursive {
 
-    public static void DFS(int n) {
+    public static void DFS_desc(int n) {
         if (n == 0) {
             return;
         } else {
-            DFS(n - 1);
+            System.out.print(n + " ");
+            DFS_asc(n - 1);
+        }
+    }
+
+    public static void DFS_asc(int n) {
+        if (n == 0) {
+            return;
+        } else {
+            DFS_desc(n - 1);
             System.out.print(n + " ");
         }
-
     }
 
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
         int n = in.nextInt();
-        DFS(n);
+        DFS_desc(n);
+        System.out.println();
+        DFS_asc(n);
     }
 }
