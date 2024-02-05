214073052ad8d0d771bfbe0507c05e8c3a69e842
eunhee <dmsgml9903@gmail.com>
2024-02-05 16:54:19 +0900

    7.14 그래프 최단거리 복습1

--- src/algorithm/Main.java
+++ src/algorithm/Main.java
@@ -1,29 +1,55 @@
 package algorithm;
 
 import java.util.Scanner;
+import java.util.List;
+import java.util.ArrayList;
+import java.util.Queue;
+import java.util.LinkedList;
 
 public class Main {
 
-  static int[] arr;
+  static List<List<Integer>> graph;
+  static int[] distance;
+  static boolean[] visit;
 
-  public static int solution(int n) {
-    if (arr[n] > 0) {
-      return arr[n];
-    }
-    if (n == 1 || n==2) {
-      return arr[n] = 1;
-    } else {
-      return arr[n] = solution(n - 2) + solution(n - 1);
+  public static void solution(int v) {
+    Queue<Integer> queue = new LinkedList<>();
+    queue.offer(v);
+    while (!queue.isEmpty()) {
+      int cv = queue.poll();
+      for (int nv : graph.get(cv)) {
+        if (!visit[nv]) {
+          visit[nv] = true;
+          queue.offer(nv);
+          distance[nv] = distance[cv] + 1;
+        }
+      }
     }
   }
 
   public static void main(String[] args) {
     Scanner in = new Scanner(System.in);
     int n = in.nextInt();
-    arr = new int[n + 1];
-    solution(n);
-    for (int i = 1; i <= n; i++) {
-      System.out.print(arr[i] + " ");
+    int m = in.nextInt();
+    graph = new ArrayList<>();
+    distance = new int[n + 1];
+    visit = new boolean[n + 1];
+    for (int i = 0; i <= n; i++) {
+      graph.add(new ArrayList<>());
+    }
+
+    for (int i = 0; i < m; i++) {
+      int start = in.nextInt();
+      int end = in.nextInt();
+      graph.get(start).add(end);
+    }
+
+    distance[1] = 0;
+    visit[1] = true;
+    solution(1);
+
+    for (int i = 2; i < n; i++) {
+      System.out.println(i + " : " + distance[i]);
     }
   }
 }
