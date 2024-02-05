d5d09f8279f9d8f8f16f0187798856a5b5debe89
eunhee <dmsgml9903@gmail.com>
2024-02-05 20:33:26 +0900

    8.2 최대점수 구하기 - 해답

--- src/algorithm/dfs/MaxScore.java
+++ src/algorithm/dfs/MaxScore.java
@@ -1,34 +1,23 @@
 package algorithm.dfs;
 
 import java.util.Scanner;
-import java.util.List;
-import java.util.ArrayList;
-
-class Problem {
-    int time;
-    int score;
-
-    public Problem(int score, int time) {
-        this.time = time;
-        this.score = score;
-    }
-}
 
 public class MaxScore {
 
-    static Problem[] problems;
-    static List<Integer> scores;
+    static int answer = Integer.MIN_VALUE;
     static int m;
     static int n;
+    static int[] scores;
+    static int[] times;
 
     public static void solution(int idx, int scoreSum, int timeSum) {
-
+        if (timeSum > m) {
+            return;
+        }
         if (idx == n) {
-            if (timeSum <= m) {
-                scores.add(scoreSum);
-            }
+                answer = Math.max(answer, scoreSum);
         } else {
-            solution(idx + 1, scoreSum + problems[idx].score, timeSum  + problems[idx].time);
+            solution(idx + 1, scoreSum + scores[idx], timeSum  + times[idx]);
             solution(idx + 1, scoreSum, timeSum);
         }
     }
@@ -37,15 +26,14 @@ public class MaxScore {
         Scanner in = new Scanner(System.in);
         n = in.nextInt();
         m = in.nextInt();
-        scores = new ArrayList<>();
-        problems = new Problem[n];
+        scores = new int[n];
+        times = new int[n];
         for (int i = 0; i < n; i++) {
-            int score = in.nextInt();
-            int time = in.nextInt();
-            problems[i] = new Problem(score, time);
+            scores[i] = in.nextInt();
+            times[i] = in.nextInt();
         }
 
         solution(0, 0, 0);
-        System.out.println(scores.stream().mapToInt(x -> x).max().getAsInt());
+        System.out.println(answer);
     }
 }
