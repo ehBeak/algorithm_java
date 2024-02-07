c867225c33f113b9b83088159919874e393a1bb8
eunhee <dmsgml9903@gmail.com>
2024-02-07 15:40:36 +0900

    8.7 조합수(메모이제이션) - 해답

--- src/algorithm/recursive/Combination.java
+++ src/algorithm/recursive/Combination.java
@@ -4,20 +4,23 @@ import java.util.Scanner;
 
 public class Combination {
 
+    static int[][] m;
+
     public static int solution(int n, int r) {
-        if (n == r) {
+        if (n == r || r == 0) {
             return 1;
         }
-        if (r == 1) {
-            return n;
+        if (m[n][r] != 0) {
+            return m[n][r];
         }
-        return solution(n - 1, r - 1) + solution(n - 1, r);
+        return m[n][r] = solution(n - 1, r - 1) + solution(n - 1, r);
     }
 
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
         int n = in.nextInt();
         int r = in.nextInt();
+        m = new int[n + 1][n + 1];
         System.out.println(solution(n, r));
     }
 }
