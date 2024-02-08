0be9772461df9a39b3bfbfa49e397b94d3790c67
eunhee <dmsgml9903@gmail.com>
2024-02-08 11:41:42 +0900

    8.7 조합의 경우의 수(메모제이션)

--- src/algorithm/recursive/Combination.java
+++ src/algorithm/recursive/Combination.java
@@ -4,23 +4,23 @@ import java.util.Scanner;
 
 public class Combination {
 
-    static int[][] m;
+    static int[][] memo;
 
     public static int solution(int n, int r) {
         if (n == r || r == 0) {
             return 1;
         }
-        if (m[n][r] != 0) {
-            return m[n][r];
+        if (memo[n][r] != 0) {
+            return memo[n][r];
         }
-        return m[n][r] = solution(n - 1, r - 1) + solution(n - 1, r);
+        return memo[n][r] = solution(n - 1, r - 1) + solution(n - 1, r);
     }
 
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
         int n = in.nextInt();
         int r = in.nextInt();
-        m = new int[n + 1][n + 1];
+        memo = new int[n + 1][n + 1];
         System.out.println(solution(n, r));
     }
 }
