98c29093ac9ae3585327335599190b9fabc5814c
eunhee <dmsgml9903@gmail.com>
2024-02-09 23:56:07 +0900

    조합 구하기

---
+++ src/algorithm/dfs/FindCombination.java
@@ -0,0 +1,32 @@
+package algorithm.dfs;
+
+import java.util.Scanner;
+
+public class FindCombination {
+
+    static int n;
+    static int m;
+    static int[] pick;
+
+    public static void solution(int level, int s) {
+        if (level == m) {
+            for (int i : pick) {
+                System.out.print(i + " ");
+            }
+            System.out.println();
+        } else {
+            for (int i = s; i <= n; i++) {
+                pick[level] = i;
+                solution(level + 1, i + 1);
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        n = in.nextInt();
+        m = in.nextInt();
+        pick = new int[m];
+        solution(0, 1);
+    }
+}
