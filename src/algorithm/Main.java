4c4a48a2ff85f605694ed302dd743b19ee6d4922
eunhee <dmsgml9903@gmail.com>
2024-02-10 21:10:16 +0900

    8.9 조합 구하기 - 복습

--- src/algorithm/Main.java
+++ src/algorithm/Main.java
@@ -1,42 +1,25 @@
 package algorithm;
 
 import java.util.Scanner;
-import java.util.Arrays;
 
 public class Main {
 
-  static int n;
-  static int m;
-  static int[] music;
+  static int n, m;
+  static int[] pick;
+  static int[] visit;
 
-  public static int count(int capacity) {
-    int sum = 0;
-    int count = 1;
-    for (int i = 0; i < n; i++) {
-      if (sum + music[i] > capacity) {
-        count++;
-        sum = music[i];
-      } else {
-        sum += music[i];
+  public static void solution(int level, int idx) {
+    if (level == m) {
+      for (int i : pick) {
+        System.out.print(i + " ");
       }
+      System.out.println();
+      return;
     }
-    return count;
-  }
-
-  public static int solution() {
-    int lt = music[n - 1];
-    int rt = Arrays.stream(music).sum();
-    int answer = Integer.MAX_VALUE;
-    while (lt <= rt) {
-      int mid = (lt + rt) / 2;
-      if (count(mid) > m) {
-        lt = mid + 1;
-      } else { // 개수가 같거나 더 적으면 용량 늘려도 된다.
-        answer = mid;
-        rt = mid - 1;
-      }
+    for (int i = idx; i <= n; i++) {
+      pick[level] = i;
+      solution(level + 1, i + 1);
     }
-    return answer;
   }
 
 
@@ -44,11 +27,8 @@ public class Main {
     Scanner in = new Scanner(System.in);
     n = in.nextInt();
     m = in.nextInt();
-    music = new int[n];
-    for (int i = 0; i < n; i++) {
-      music[i] = in.nextInt();
-    }
-    Arrays.sort(music);
-    System.out.println(solution());
+    pick = new int[m];
+    visit = new int[n];
+    solution(0, 1);
   }
 }
