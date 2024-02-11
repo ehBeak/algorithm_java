a93f9e63750eacdda26dfe410f014f461f370f8e
eunhee <dmsgml9903@gmail.com>
2024-02-11 14:21:16 +0900

    8.9 조합 구하기 - 복습

--- src/algorithm/Main.java
+++ src/algorithm/Main.java
@@ -4,21 +4,44 @@ import java.util.Scanner;
 
 public class Main {
 
-  static int n, m;
-  static int[] pick;
-  static int[] visit;
+  static int n, f;
+  static int[] combination;
+  static int[][] memo;
+  static int[] arr;
+  static boolean[] visit;
+  static boolean flag = false;
 
-  public static void solution(int level, int idx) {
-    if (level == m) {
-      for (int i : pick) {
-        System.out.print(i + " ");
-      }
-      System.out.println();
+  public static int C(int n, int r) {
+    if (n == r || r == 0) {
+      return 1;
+    }
+    if (memo[n][r] != 0) {
+      return memo[n][r];
+    }
+    return memo[n][r] = C(n - 1, r - 1) + C(n - 1, r);
+  }
+
+  public static void solution(int level, int sum) {
+    if (flag) {
       return;
     }
-    for (int i = idx; i <= n; i++) {
-      pick[level] = i;
-      solution(level + 1, i + 1);
+    if (level == n) {
+      if (sum == f) {
+        flag = true;
+        for (int i : arr) {
+          System.out.print(i + " ");
+        }
+        System.out.println();
+      }
+    }
+
+    for (int i = 1; i <= n; i++) {
+      if (!visit[i]) {
+        visit[i] = true;
+        arr[level] = i;
+        solution(level + 1, sum + (arr[level] * combination[level]));
+        visit[i] = false;
+      }
     }
   }
 
@@ -26,9 +49,14 @@ public class Main {
   public static void main(String[] args) {
     Scanner in = new Scanner(System.in);
     n = in.nextInt();
-    m = in.nextInt();
-    pick = new int[m];
-    visit = new int[n];
-    solution(0, 1);
+    f = in.nextInt();
+    combination = new int[n];
+    arr = new int[n];
+    memo = new int[n][n];
+    visit = new boolean[n + 1];
+    for (int i = 0; i < n; i++) {
+      combination[i] = C(n - 1, i);
+    }
+    solution(0, 0);
   }
 }
