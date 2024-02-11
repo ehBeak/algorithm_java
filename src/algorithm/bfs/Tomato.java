4b8258e06f716b08cdde128c71ddb61ba0c849cc
eunhee <dmsgml9903@gmail.com>
2024-02-11 20:45:15 +0900

    8.12 토마토 복습

--- src/algorithm/bfs/Tomato.java
+++ src/algorithm/bfs/Tomato.java
@@ -13,58 +13,63 @@ class Point {
     }
 }
 
-
 public class Tomato {
 
-    static int n, m;
+    static int m, n, answer = Integer.MIN_VALUE;
     static int[][] box;
     static int[][] days;
-    static int[] dx = {-1, 0, 1, 0};
-    static int[] dy = {0, -1, 0, 1};
+    static Queue<Point> queue = new LinkedList<>();
 
-    public static final int NOTHING = -1;
+    static int[] dx = {1, 0, -1, 0};
+    static int[] dy = {0, 1, 0, -1};
+
     public static final int RIPEN = 1;
     public static final int NOT_RIPEN = 0;
+    public static final int NOTHING = -1;
+
+    public static void bfs() {
+        while (!queue.isEmpty()) {
+            Point point = queue.poll();
+            for (int k = 0; k < 4; k++) {
+                int nx = point.x + dx[k];
+                int ny = point.y + dy[k];
+                if (nx >= 0 && nx < n && ny >= 0 && ny < m && box[nx][ny] == NOT_RIPEN) {
+                    box[nx][ny] = RIPEN;
+                    days[nx][ny] = days[point.x][point.y] + 1;
+                    queue.offer(new Point(nx, ny));
+                }
+            }
+        }
+
+    }
 
     public static int solution() {
-        int max = Integer.MIN_VALUE;
-        Queue<Point> queue = new LinkedList<>();
         for (int i = 0; i < n; i++) {
             for (int j = 0; j < m; j++) {
                 if (box[i][j] == RIPEN) {
-                    queue.add(new Point(i, j));
+                    queue.offer(new Point(i, j));
                 }
             }
         }
-        while (!queue.isEmpty()) {
-            Point p = queue.poll();
-            for (int k = 0; k < 4; k++) {
-                int nx = p.x + dx[k];
-                int ny = p.y + dy[k];
-                if (nx >= 0 && nx < n && ny >= 0 && ny < m) {
-                    if (box[nx][ny] == NOT_RIPEN) {
-                        box[nx][ny] = RIPEN;
-                        days[nx][ny] = days[p.x][p.y] + 1;
-                        queue.offer(new Point(nx, ny));
-                    }
-                }
-            }
-        }
+        bfs();
+
         for (int i = 0; i < n; i++) {
             for (int j = 0; j < m; j++) {
-                max = Math.max(days[i][j], max);
+                answer = Math.max(answer, days[i][j]);
                 if (box[i][j] == NOT_RIPEN) {
                     return -1;
                 }
             }
         }
-        return max;
+
+        return answer;
     }
+
 
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
-        m = in.nextInt(); // 세로 칸 수
-        n = in.nextInt(); // 가로 칸 수
+        m = in.nextInt();
+        n = in.nextInt();
         box = new int[n][m];
         days = new int[n][m];
         for (int i = 0; i < n; i++) {
